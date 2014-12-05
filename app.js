5299870f022f0fd4e81dc0b5f84fccc395575444
sengwook <sengwook@sengwook-VirtualBox>
2014-12-05 18:05:44 +0900

    Blog 10%

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
  */
 
 var express = require('express')
+  , sys = require('sys')
   , logger = require('morgan')
   , bodyParser = require('body-parser')
   , favicon = require('static-favicon')
@@ -16,24 +17,30 @@ var app = express();
 // all environments
 app.set('port', process.env.PORT || 12080);
 app.set('views', __dirname + '/views');
+app.engine('jade', require('jade').__express);
 app.set('view engine', 'jade');
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }))    // parse application/x-www-form-urlencoded
 app.use(bodyParser.json())    // parse application/json
 app.use(methodOverride());
+console.log("PASS METHOD OVERRIDE");
+
 //app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-	app.use(errorHandler());
+	//app.use(errorHandler());
 }
 
-require("./routes/main")(app);
 
+
+require("./routes/main")(app);
+require("./app/menus")();
+require("./app/error")(app,sys);
 app.use(function(req, res, next) {
-	console.log("CORS ALLOW" , req);
+  //console.log("CORS ALLOW" , req.headers);
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
--- routes/main.js
+++ routes/main.js
@@ -1,13 +1,94 @@
 module.exports = function(app) {
 	// 메이페이지 지정
-	app.get('/blog',function ( req , res) {
-		console.log("ERNDER INDEX");
-		res.render('blog', { title: 'blog' });
+	var blogArticle = require("../app/db/database")(app);
+		
+	app.get('/blog/update/:id',function(req, res) {
+		console.log("Blog",req.params.user,req.params.content);
+		var moment = require('moment');
+		blogArticle.find().limit(3).sort({'regdt':-1}).exec(function (arr,data) {
+			console.log(data);
+			res.render('blog', withMenu({
+				
+				title : 'Index',
+				type : 'edit',
+				article : {
+					id : '',
+					title : 'NEW TITLE',
+					content : '<DIV> HELLO WORLD</DIV>',
+					public : 'false',
+					regdt : new Date(),
+					uptdt : new Date()
+				},
+				moment : moment
+			}));
+
+		});		
 	});
 	
-	app.get('/',function ( req , res) {
-		console.log("ERNDER INDEX");
-		res.render('index', { title: 'blog' });
+	app.get('/blog',function(req, res) {
+		console.log("Blog",req.params.user,req.params.content);
+		blogArticle.find().limit(3).sort({'regdt':-1}).exec(function (arr,data) {
+			console.log(data);
+			var moment = require('moment');
+			res.render('blog', withMenu({
+				title : 'Index',
+				type : 'display' ,
+				articles : data ,
+				moment : moment
+			}));
+
+		});		
+	});
+	app.get('/blog/:user/',function(req, res) {
+		console.log("Blog",req.params.user,req.params.content);
+		res.render('blog', withMenu({
+			title : 'Blog'
+		}));
+	});
+	app.get('/blog/:user/:content', function(req, res) {
+		console.log("Blog",req.params.user,req.params.content);
+		res.render('blog', withMenu({
+			title : 'Blog'
+		}));
 	});
 	
+	app.get('/video', function(req, res) {
+		res.render('video', withMenu({
+			title : 'Video'
+		}));
+	});
+	app.post('/blog' , function (req,res) {
+		console.log(blogArticle);
+		
+		blogArticle.find().limit(3).sort({'regdt':-1}).exec(function (arr,data) {
+			console.log(data);
+			
+			res.send(data);
+		});
+	});
+	
+	
+	app.put('/blog' , function (req,res) {
+		
+		console.log(req.body);
+		var model = new blogArticle();
+		model.apply(req.body);
+		model.regdt = new Date();
+		model.uptdt = new Date();
+		model.save();
+		
+		res.send({rslt:'success'});
+	});
+	
+	app.get('/err', function(req, res) {
+		throw new ZException("ERROR");
+	});
+	
+	app.get('/', function(req, res) {
+		console.log("ERNDER INDEX");
+		console.log(blogArticle);
+		res.render('index', withMenu({
+			title : 'index'
+		}));
+	});
 }
